bf2b03780784927616fb74e98a41db67d585f3d1
EliteHacker228 <mchappycraft@yandex.ru>
2024-03-05 20:58:52 +0500

    Init commit

---
+++ src/main/java/ru/maeasoftoworks/normativecontrol/api/NormativeControlApiApplication.java
@@ -0,0 +1,13 @@
+package ru.maeasoftoworks.normativecontrol.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormativeControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormativeControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/maeasoftoworks/normativecontrol/api/NormativeControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maeasoftoworks.normativecontrol.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormativeControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
