1f550ec6f977ab12385a69db533405ab3101ba1d
EliteHacker228 <mchappycraft@yandex.ru>
2024-03-11 20:33:58 +0500

    Add files status checking and results' downloading

--- src/main/java/ru/maeasoftoworks/normativecontrol/api/controllers/DocumentController.java
+++ src/main/java/ru/maeasoftoworks/normativecontrol/api/controllers/DocumentController.java
@@ -2,12 +2,11 @@ package ru.maeasoftoworks.normativecontrol.api.controllers;
 
 import io.minio.*;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import net.minidev.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.maeasoftoworks.normativecontrol.api.mq.DocumentMessageBody;
 import ru.maeasoftoworks.normativecontrol.api.mq.MqPublisher;
@@ -22,14 +21,76 @@ public class DocumentController {
 
     private MqPublisher mqPublisher;
 
-    @GetMapping("/verification")
+    @PostMapping("/verification")
     public ResponseEntity<String> sendToVerification(@RequestParam(name = "document", required = true) MultipartFile file) throws IOException {
         DocumentMessageBody resultFileName = s3UploadFile(file.getInputStream());
         mqPublisher.publishToVerify(resultFileName.getAsJsonString(), resultFileName.getCorrelationId());
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("documentId", resultFileName.getCorrelationId());
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(resultFileName.getAsJsonString());
+                .body(jsonObject.toJSONString());
+    }
+
+    @GetMapping("/isVerified")
+    public ResponseEntity<String> isDocumentVerified(@RequestParam String documentId) {
+        boolean result = s3isDocumentVerificationExists(documentId);
+        JSONObject jsonObject = new JSONObject();
+
+        if (result) {
+            jsonObject.put("message", "document with id " + documentId + " is verified");
+
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonObject.toJSONString());
+        } else {
+            jsonObject.put("message", "document with id " + documentId + " is not verified");
+
+            return ResponseEntity
+                    .badRequest()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonObject.toJSONString());
+        }
+    }
+
+    @GetMapping("/verifiedDocument")
+    @SneakyThrows
+    public ResponseEntity<byte[]> isDocumentVerified(@RequestParam String documentId, @RequestParam String documentType) {
+        InputStream result = s3getVerificationResult(documentId, documentType);
+
+        if (result != null) {
+
+            if (documentType.equals("docx"))
+                return ResponseEntity
+                        .ok()
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .body(result.readAllBytes());
+            else if(documentType.equals("html"))
+                return ResponseEntity
+                        .ok()
+                        .contentType(MediaType.TEXT_HTML)
+                        .body(result.readAllBytes());
+            else {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("message", "Result of type " + documentType + " is not exists");
+
+                return ResponseEntity
+                        .badRequest()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(jsonObject.toJSONString().getBytes("UTF-8"));
+            }
+        } else {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("message", "Result with of type " + documentType + " is not exists");
+
+            return ResponseEntity
+                    .badRequest()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonObject.toJSONString().getBytes("UTF-8"));
+        }
     }
 
     private DocumentMessageBody s3UploadFile(InputStream inputStream) {
@@ -74,4 +135,89 @@ public class DocumentController {
         }
         return null;
     }
+
+    private boolean s3isDocumentVerificationExists(String documentId) {
+        try {
+            final String BUCKET_NAME = "normative-control";
+            final String BUCKET_ENDPOINT = "http://localhost:9000";
+            final String BUCKET_ACCESS_KEY = "pdh5niPCbqYxGW5BKxGv";
+            final String BUCKET_SECRET_KEY = "xqtc6gXpKy0OIZVVSeinxDd7dd0pbDsOEtU7huJX";
+
+            // Create a minioClient with the MinIO server playground, its access key and secret key.
+            MinioClient minioClient =
+                    MinioClient.builder()
+                            .endpoint(BUCKET_ENDPOINT)
+                            .region("US-EAST-1")
+                            .credentials(BUCKET_ACCESS_KEY, BUCKET_SECRET_KEY)
+                            .build();
+
+            // Make 'normative-control' bucket if not exist.
+            boolean found =
+                    minioClient.bucketExists(BucketExistsArgs.builder().bucket(BUCKET_NAME).region("US-EAST-1").build());
+            if (!found) {
+                // Make a new bucket called 'normative-control'.
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(BUCKET_NAME).region("US-EAST-1").build());
+            } else {
+                System.out.println("Bucket 'normative-control' already exists.");
+            }
+
+            String target1 = documentId + "/result.docx";
+            String target2 = documentId + "/result.html";
+
+            minioClient.statObject(StatObjectArgs.builder()
+                    .bucket(BUCKET_NAME).region("US-EAST-1")
+                    .object(target1)
+                    .build());
+
+            minioClient.statObject(StatObjectArgs.builder()
+                    .bucket(BUCKET_NAME).region("US-EAST-1")
+                    .object(target2)
+                    .build());
+
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e);
+            return false;
+        }
+    }
+
+    private InputStream s3getVerificationResult(String documentId, String documentType) {
+        try {
+            final String BUCKET_NAME = "normative-control";
+            final String BUCKET_ENDPOINT = "http://localhost:9000";
+            final String BUCKET_ACCESS_KEY = "pdh5niPCbqYxGW5BKxGv";
+            final String BUCKET_SECRET_KEY = "xqtc6gXpKy0OIZVVSeinxDd7dd0pbDsOEtU7huJX";
+
+            // Create a minioClient with the MinIO server playground, its access key and secret key.
+            MinioClient minioClient =
+                    MinioClient.builder()
+                            .endpoint(BUCKET_ENDPOINT)
+                            .region("US-EAST-1")
+                            .credentials(BUCKET_ACCESS_KEY, BUCKET_SECRET_KEY)
+                            .build();
+
+            // Make 'normative-control' bucket if not exist.
+            boolean found =
+                    minioClient.bucketExists(BucketExistsArgs.builder().bucket(BUCKET_NAME).region("US-EAST-1").build());
+            if (!found) {
+                // Make a new bucket called 'normative-control'.
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(BUCKET_NAME).region("US-EAST-1").build());
+            } else {
+                System.out.println("Bucket 'normative-control' already exists.");
+            }
+
+            String target = documentId + "/result." + documentType;
+
+            InputStream object = minioClient.getObject(GetObjectArgs.builder()
+                    .bucket(BUCKET_NAME).region("US-EAST-1")
+                    .object(target)
+                    .build());
+
+            return object;
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e);
+            return null;
+        }
+    }
+
 }
