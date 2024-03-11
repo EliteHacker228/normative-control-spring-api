fb21347764868f07eb20aee03cb4f35627792f58
EliteHacker228 <mchappycraft@yandex.ru>
2024-03-12 01:50:19 +0500

    More or less working version

--- src/main/java/ru/maeasoftoworks/normativecontrol/api/security/WebSecurityConfig.java
+++ src/main/java/ru/maeasoftoworks/normativecontrol/api/security/WebSecurityConfig.java
@@ -55,3 +55,35 @@
 //        return authProvider;
 //    }
 //}
+
+package ru.maeasoftoworks.normativecontrol.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig implements WebMvcConfigurer {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(request ->
+                                        new CorsConfiguration().applyPermitDefaultValues()
+                                )
+                )
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
