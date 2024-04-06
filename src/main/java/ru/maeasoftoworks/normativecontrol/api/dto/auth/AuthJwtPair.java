f9c6279963f92c5a2b3708d7594d4f1e0c26d230
EliteHacker228 <mchappycraft@yandex.ru>
2024-04-07 04:34:27 +0500

    Remove redundant entity

--- src/main/java/ru/maeasoftoworks/normativecontrol/api/dto/auth/AuthJwtPair.java
+++ src/main/java/ru/maeasoftoworks/normativecontrol/api/dto/auth/AuthJwtPair.java
@@ -2,11 +2,10 @@ package ru.maeasoftoworks.normativecontrol.api.dto.auth;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import ru.maeasoftoworks.normativecontrol.api.dto.JsonSerializable;
 
 @RequiredArgsConstructor
 @Getter
-public class AuthJwtPair extends JsonSerializable {
+public class AuthJwtPair {
     private final String accessToken;
     private final String refreshToken;
 }
--- src/main/java/ru/maeasoftoworks/normativecontrol/api/dto/auth/login/LoginData.java
+++ src/main/java/ru/maeasoftoworks/normativecontrol/api/dto/auth/login/LoginData.java
@@ -2,11 +2,10 @@ package ru.maeasoftoworks.normativecontrol.api.dto.auth.login;
 
 import lombok.Getter;
 import lombok.Setter;
-import ru.maeasoftoworks.normativecontrol.api.dto.JsonSerializable;
 
 @Getter
 @Setter
-public class LoginData extends JsonSerializable {
+public class LoginData {
     private String email;
     private String password;
 }
