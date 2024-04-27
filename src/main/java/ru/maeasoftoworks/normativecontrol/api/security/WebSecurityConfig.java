e2f4c0e3eb9d957df86b85eccac4207e1101165d
EliteHacker228 <mchappycraft@yandex.ru>
2024-04-27 05:25:47 +0500

    Add security config

--- src/main/java/ru/maeasoftoworks/normativecontrol/api/security/WebSecurityConfig.java
+++ src/main/java/ru/maeasoftoworks/normativecontrol/api/security/WebSecurityConfig.java
@@ -2,19 +2,28 @@ package ru.maeasoftoworks.normativecontrol.api.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.maeasoftoworks.normativecontrol.api.domain.users.Role;
+
 import java.util.List;
-import org.springframework.security.authentication.AuthenticationManager;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +31,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSecurityConfig implements WebMvcConfigurer {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -34,8 +46,58 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                     corsConfiguration.setAllowedHeaders(List.of("*"));
                     corsConfiguration.setAllowCredentials(true);
                     return corsConfiguration;
-                }));
+                }))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/register/normocontroller").hasRole(Role.ADMIN.name())
+                        .requestMatchers("/auth/**").anonymous()
+                        .requestMatchers("/auth/tokens").authenticated()
+
+                        .requestMatchers("/accounts").hasRole(Role.ADMIN.name())
+                        .requestMatchers("/accounts/**").authenticated()
+                        .requestMatchers("/accounts/*/documents-limit").hasRole(Role.ADMIN.name())
+
+                        .requestMatchers("/academical/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/academical/groups").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.PATCH,"/academical/groups").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE,"/academical/groups").hasRole(Role.ADMIN.name())
+                        .requestMatchers("/academical/groups/students").hasAnyRole(Role.NORMOCONTROLLER.name(), Role.ADMIN.name())
+
+                        .requestMatchers("/invites/**").hasAnyRole(Role.NORMOCONTROLLER.name(), Role.ADMIN.name())
+
+                        .requestMatchers("/documents/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/documents").hasRole(Role.STUDENT.name())
+                        .requestMatchers(HttpMethod.GET, "/documents").authenticated()
+
+                        .requestMatchers("/error", "/error/**").permitAll())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    // TODO: Использовать поставляемый спрингом энкодер, а не собтсвенный через sha256
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return rawPassword.toString().equals(encodedPassword);
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        log.info("AUTH PROVIDER: " + authProvider);
+        return authProvider;
     }
 
     @Bean
@@ -44,4 +106,3 @@ public class WebSecurityConfig implements WebMvcConfigurer {
         return config.getAuthenticationManager();
     }
 }
-
