14e6bfe2c0de5dfa260e813619827d8e941db91d
EliteHacker228 <mchappycraft@yandex.ru>
2024-04-30 07:24:04 +0500

    Fix basic crud test

--- src/test/java/ru/maeasoftoworks/normativecontrol/api/db/BasicCrudTests.java
+++ src/test/java/ru/maeasoftoworks/normativecontrol/api/db/BasicCrudTests.java
@@ -109,8 +109,6 @@ public class BasicCrudTests {
             Document document = Document.builder()
                     .student(student)
                     .fileName("И.А.Шарапов РИ-400015 ВКР")
-                    .academicGroup(RI_400015)
-                    .studentName("Шарапов И.А.")
                     .isReported(false)
                     .comment("")
                     .build();
